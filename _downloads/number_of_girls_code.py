88c983d2fc47854135679c53332c82c27c862f31
Matthew Brett <matthew.brett@gmail.com>
2017-09-17 01:02:34 +0100

    Update documentation

---
+++ _downloads/number_of_girls_code.py
@@ -0,0 +1,28 @@
+""" Three girls in a family of four
+"""
+#: We need the random module
+import random
+
+#: A function for a single coin toss
+def coin_toss():
+    random_no = random.random()
+    if random_no <= 0.5:
+        return 1
+    return 0
+
+
+#- Test the coin toss a few times
+
+#- Make a list, and collect four coin tosses
+
+#- Sum the list to give the number of girls in this family
+
+#- Make a family from coin tosses, using a for loop.
+
+#- Make a list to store the number of girls in each family
+#- Use a for loop to make 1000 families.
+#- For each family, calculate the number of girls
+#- Store in the list
+#- Count the number of families with 3 girls
+
+#- divide the number of familes with 3 girls by the number of families
---
+++ _sources/programs_in_python.rst.txt
@@ -0,0 +1,10 @@
+#####################
+Programming in Python
+#####################
+
+* Revision on lists - append, count
+* Sum
+* If statements
+* For loops
+* Range
+* Random, again
